66205c40be4d18d26d89388fc52bc3b4d0262cfa
aberlanga <alexcberlanga@gmail.com>
2016-07-22 10:26:49 -0400

    Add files via upload

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os, sys
 
 # Pre: Requires correct directory of benchmarks in variable path
 # Post: Creates list of benchmarks
-path = '/home/aberlanga1/Desktop/ONoC-Simulation-master/Benchmark Data/16-core'
+path = 'C:\\Users\\NinetyBear\\Documents\\Visual Studio 2015\\Projects\\ONoC Simulator\\PythonApplication1\\Benchmark Data\\16-core'
 dirs = os.listdir(path)
 benchmarks = []
 benchmarksOnly = []
@@ -14,7 +14,7 @@ for file in dirs:
     benchmarks.append(file)
     benchmarksOnly.append(file)
 for i in range(0,len(benchmarks)):
-    benchmarks[i] = path + '/' + benchmarks[i]
+    benchmarks[i] = path + '\\' + benchmarks[i]
 activeReq = []
 nodestate = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 isover = False
--- simulation.py
+++ simulation.py
@@ -2,6 +2,7 @@ from request import request
 import config
 import time
 import random
+import nn
 
 #Converts a log file of XY coordinates to node numbers and returns a reduced list of lists that contains the log information in its elements.
 #Ex.     S|D |#|V|tStamp
@@ -50,46 +51,95 @@ def generateKey(nodeCount):
             key.append(randNum)
     return key
 
-def writeResults(logFile, tClocks,tSimulation, tProgram):
-    with open('results.log',"a") as resultFile:
-        resultFile.write(logFile + '    ' + str(tClocks) + '    ' + str(tSimulation) + '    ' + str(tProgram) + '\n')
 
-#key1  = generateKey(config.nodeCount)         
+def mapping(key, benchmark):
+    for row in benchmark:
+        row[0]=key[row[0]]
+        row[1]=key[row[1]]
+
+        return benchmark
+
+
+
+
+
+
+
+
+
+
+
+def writeResults(logFile, tClocks,tSimulation, tProgram, currentReconfiguration):
+    with open('results.log',"a") as resultFile:
+        resultFile.write(logFile + '    ' + currentReconfiguration + str(tClocks) + '    ' + str(tSimulation) + '    ' + str(tProgram) + '\n')
+
+
+
+
+
+
+
+randKey1  = generateKey(config.nodeCount)         
+randKey2  = generateKey(config.nodeCount)
+randKey3  = generateKey(config.nodeCount)
+currentReconfiguration = ""
 
 for i in range(0,len(config.benchmarks)):
-    startTime = time.time()
     logFile = config.benchmarks[i]    
-    nodeBenchmarkList = convXYtoNode(logFile)
+    ConstnodeBenchmarkList = convXYtoNode(logFile)
     listLen = len(nodeBenchmarkList)
     print config.benchmarksOnly[i]
     print '-----------------' + '\n'
     print 'ListLength:' + str(listLen)
-    reqCount = 0
-    endFlag = False
-    config.isover = False
-    config.nodestate = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    for reconfigs in range(0,5):
+        startTime = time.time()
+        nodeBenchmarkList = ConstnodeBenchmarkList
+        reqCount = 0
+        endFlag = False
+        config.isover = False
+        config.nodestate = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 
-    t = 0
+        t = 0
+        if reconfigs == 0:
+            currentReconfiguration = "Numerical"
 
-    while config.isover == False:
-        while  (t == nodeBenchmarkList[reqCount][4] and endFlag==False):
-            if reqCount+1 == listLen:
-                endFlag = True
-            config.activeReq.append(request(nodeBenchmarkList[reqCount][0],nodeBenchmarkList[reqCount][1],nodeBenchmarkList[reqCount][3],nodeBenchmarkList[reqCount][4],nodeBenchmarkList[reqCount][4],endFlag))
-            if endFlag == False:
-                reqCount += 1
+        elif reconfigs == 1:
+            nodeBenchmarkList = mapping(randKey1,nodeBenchmarkList)
+            currentReconfiguration = "Random1"
 
-        for req in config.activeReq:
-            req.reqProcessing(t)
-        t += 1
+        elif reconfigs == 2:
+            nodeBenchmarkList = mapping(randKey2,nodeBenchmarkList)
+            currentReconfiguration = "Random2"
+
+        elif reconfigs == 3:
+            nodeBenchmarkList = mapping(randKey3,nodeBenchmarkList)
+            currentReconfiguration = "Random3"
+
+        elif reconfigs == 4:
+            nNKey = nn.nearestNeighbourKeygen(nodeBenchmarkList)
+            nodeBenchmarkList = mapping(nNKey,nodeBenchmarkList)
+            currentReconfiguration = "nNeighbor"
+
+        while config.isover == False:
+            while  (t == nodeBenchmarkList[reqCount][4] and endFlag==False):
+                if reqCount+1 == listLen:
+                    endFlag = True
+                config.activeReq.append(request(nodeBenchmarkList[reqCount][0],nodeBenchmarkList[reqCount][1],nodeBenchmarkList[reqCount][3],nodeBenchmarkList[reqCount][4],nodeBenchmarkList[reqCount][4],endFlag))
+                if endFlag == False:
+                    reqCount += 1
+
+            for req in config.activeReq:
+                req.reqProcessing(t)
+            t += 1
 
 
+        tProgram = time.time()-startTime
+        print config.nodestate 
+        print '\n'
+        print "Reconfiguration: " + currentReconfiguration
+        print 'Optical Clock Cycles: '+ str(t)
+        totalTime = float(t)/(config.OCC*(10**9))
+        print 'Runtime: '+ str(totalTime) +'seconds' 
+        print 'Time for Traffic: ' + str(tProgram)
 
-    print config.nodestate
-    print 'Optical Clock Cycles: '+ str(t)
-    totalTime = float(t)/(config.OCC*(10**9))
-    print 'Runtime: '+ str(totalTime) +'seconds'
-    tProgram = time.time()-startTime
-    print 'Time for Program: ' + str(tProgram)
-
-    writeResults(config.benchmarksOnly[i], t,totalTime, tProgram)
+        writeResults(config.benchmarksOnly[i], t,totalTime, tProgram,currentReconfiguration)
